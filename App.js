24a01afe214785375602a1b56661b16b1adb4bf8
Tobias Schmücker <schmuecker.tobias@gmail.com>
2020-03-21 19:24:30 +0100

    add react-navigation

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,11 +11,13 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      </NavigationContainer>
     );
   }
 }
