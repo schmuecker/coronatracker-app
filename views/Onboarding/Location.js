5baa483c1e25046fc2b3284cb6063b109d96e02d
Tobias Schmücker <schmuecker.tobias@gmail.com>
2020-03-22 10:00:20 +0100

    Add Onboard Location screen

---
+++ views/Onboarding/Location.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import * as ExpoLocation from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+
+export default Location = () => {
+
+  const [error, setError] = useState(undefined);
+  const [location, setLocation] = useState(undefined);
+
+  // This tests the location permission and gets the current position.
+  // Use this method to track the location in the background: https://docs.expo.io/versions/latest/sdk/location/#locationstartlocationupdatesasynctaskname-options
+  const getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      setError('Permission to access location was denied');
+    }
+  
+    let location = await ExpoLocation.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
+  useEffect(() => {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      setError('Oops, this will not work on Sketch in an Android emulator. Try it on your device!');
+    } else {
+      getLocationAsync();
+    }
+  }, []);
+
+  return (
+      <View style={styles.container}>
+          <View style={styles.content}>
+            {
+              !location && 
+              <>
+              <Text style={styles.title}>Einige Funktionen sind ohne vollständigen Standortzugriff nicht verfügbar.</Text>
+              <Text style={styles.title}>Erlauben Sie den Standortzugriff, um die App in vollem Umfang nutzen können.</Text>
+              <Button style={styles.button} title="Standortzugriff erlauben" />
+              </>
+            }
+            {
+              location &&
+              <Text style={styles.title}>Standortzugriff erlaubt 👍</Text>
+            }
+            {
+              error &&
+              <Text style={styles.title}>{error}</Text>
+            }
+          </View>
+          <View style={styles.actions}>
+            <Button style={styles.button} title="Zurück" />
+            <Button style={styles.button} title="Weiter" />
+          </View>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    margin: 20
+  }
+});
--- views/Onboarding/index.js
+++ views/Onboarding/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Button, StyleSheet, View } from 'react-native';
-import Welcome from './Welcome';
+import Location from './Location';
 
 export default class Onboarding extends Component {
   render() {
     return (
-        <Welcome />
+        <Location />
     );
   }
 }
